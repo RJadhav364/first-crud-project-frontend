c5e5fae6078120f66ba7e05d3c819580623b63dd
Rohan@Quanticteq <rohan@quanticteq.com>
2024-12-11 18:46:54 +0530

    front end project set up and running correctly

---
+++ src/routes/Approutes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Admin from '../layout/Admin'
+import Login from '../pages/Login'
+
+const Approutes = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="admin" element={<Admin />}>
+        </Route>
+    </Routes>
+  )
+}
+
+export default Approutes
