22090c801d9e73ede2bfd1e9bbc9de091e2eeb7e
Rohandesk <rohandesk372@gmail.com>
2025-03-05 12:27:17 +0530

    ngrok api end point added

--- config/app.config.js
+++ config/app.config.js
@@ -1,3 +1,4 @@
+// const Service_url = `https://8afe-103-87-55-47.ngrok-free.app/`;
 const Service_url = `http://localhost:9000/`;
 // const Service_url = `https://first-crud-project-backend.onrender.com/`;
 
